2e4400abb8cb0539181d65190903ca31b39bae74
Hussein-Mansour <74609841+Hussein-Mansour@users.noreply.github.com>
2021-11-24 18:40:25 -0500

    Update GuessGame.java

--- GuessGame.java
+++ GuessGame.java
@@ -5,14 +5,27 @@
 * @since 2020-11-23
 */
 
-import java.util.Scanner;
 import java.util.Random;
+import java.util.Scanner;
 
 /**
 * This is the standard "GuessGame" program.
 */
 
 final class GuessGame {
+
+    /**
+    * Declaring exponent variable.
+    */
+    public static final int NUMB = 7;
+    /**
+    * Declaring exponent variable.
+    */
+    public static final int NUMBB = 6;
+    /**
+    * Declaring exponent variable.
+    */
+    public static final String SPACE = "\n";
 
     /**
     * Prevent instantiation.
@@ -40,15 +53,15 @@ final class GuessGame {
         try {
 
             // while loop to generate random number 1-6
-            Random rand = new Random();
+            final Random rand = new Random();
             int counter = 1;
-            while (counter < 7) {
+            while (counter < NUMB) {
                 counter = counter + 1;
-                }
-            int randomNumber = rand.nextInt(counter);
+            }
+            final int randomNumber = rand.nextInt(counter);
 
-            // creating Scanner 
-            Scanner myObj = new Scanner(System.in);
+            // creating Scanner
+            final Scanner myObj = new Scanner(System.in);
             // while for guess again and types
             int numberOfTries = 0;
             while (true) {
@@ -56,33 +69,29 @@ final class GuessGame {
                 final int userInput = myObj.nextInt();
                 numberOfTries = numberOfTries + 1;
 
-                // if statment                                                      
-                if (userInput == randomNumber) { 
-                    System.out.print("\n" + "You are correct!" 
-                                          + "\n" + "It took you "     
-                                          + numberOfTries + " tries.");
-                } else if (userInput > 6) {
-                    System.out.print("This isn't between 1 to 6." 
-                                       + "This won't count" + "\n"
-                                       + "\n");
-                } else if (userInput < randomNumber) { 
-                    System.out.print("You guessed too Low!" + "\n" 
-                                       + "Guess again" + "\n"
-                                       + "\n"); 
-                } else if (userInput > randomNumber) {
-                    System.out.print( "You guessed too high!" + "\n" 
-                                       + "Guess again" + "\n"
-                                       + "\n");
-                }
-
-                // break
-                if (randomNumber == userInput) {
+                // if statment
+                if (userInput == randomNumber) {
+                    System.out.print(SPACE + "You are correct!"
+                                           + SPACE + "It took you "
+                                           + numberOfTries + " tries." + SPACE);
                     break;
+                } else if (userInput > NUMBB) {
+                    System.out.print("This isn't between 1 to 6."
+                                       + "This won't count" + SPACE
+                                       + SPACE);
+                } else if (userInput < randomNumber) {
+                    System.out.print("You guessed too Low!" + SPACE
+                                       + "Guess again " + SPACE
+                                       + SPACE);
+                } else if (userInput > randomNumber) {
+                    System.out.print("You guessed too high!" + SPACE
+                                       + "Guess again" + SPACE
+                                       + SPACE);
                 }
             }
-        } catch (java.util.InputMismatchException errorCode) { 
+        } catch (java.util.InputMismatchException errorCode) {
             // Invalid Input.
-            System.out.println("Invalid Input.");
+            System.out.println("This is not an integer.");
         }
         System.out.println("\nDone.");
     }
