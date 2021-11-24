524d6505f3f70af8264442b89da6296c2da1a0c7
Ubuntu <ubuntu@ip-172-31-95-135.ec2.internal>
2021-11-24 17:36:25 +0000

    added GuessGame.java(final)

--- GuessGame.java
+++ GuessGame.java
@@ -63,12 +63,21 @@ final class GuessGame {
 
                 // if statment                                                      
                 if (userInput == randomNumber) { 
-                    System.out.print("You are correct!" + "\n" + "It took you "     
-                                       + numberOfTries + " tries.");
+                    System.out.print("\n" + "You are correct!" 
+                                          + "\n" + "It took you "     
+                                          + numberOfTries + " tries.");
+                } else if (userInput > 6) {
+                    System.out.print("This isn't between 1 to 6." 
+                                       + "This won't count" + "\n"
+                                       + "\n");
                 } else if (userInput < randomNumber) { 
-                    System.out.print("You guessed too Low!" + "\n" +"Guess again"); 
+                    System.out.print("You guessed too Low!" + "\n" 
+                                       + "Guess again" + "\n"
+                                       + "\n"); 
                 } else if (userInput > randomNumber) {
-                    System.out.print("You guessed too high!" + "\n" +"Guess again");
+                    System.out.print( "You guessed too high!" + "\n" 
+                                       + "Guess again" + "\n"
+                                       + "\n");
                 }
 
                 // break
